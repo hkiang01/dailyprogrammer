bc06dabffa5698bd738bb868fe335b5cbb4d1a47
Harrison Kiang <hkiang01@gmail.com>
2021-02-07 21:23:52 -0600

    my first go program

---
+++ 2012-02-10/easy/dailyprogrammer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func prompt(prompt string) string {
+	fmt.Println(prompt)
+	var field string
+	_, err := fmt.Scanln(&field)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return field
+}
+
+func main() {
+	name := prompt("What is your name?")
+	ageStr := prompt("How old are you?")
+	age, err := strconv.Atoi(ageStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	user := prompt("What is your username?")
+	fmt.Println(fmt.Sprintf("Your name is %s, you are %s years old, and your username is %s", name, age, user))
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# dailyprogrammer
+
+See https://www.reddit.com/r/dailyprogrammer/wiki/challenges#wiki_challenge
+
