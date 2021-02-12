a4898595718d965afc38b71c9fab53e2c541037a
Harrison Kiang <hkiang01@gmail.com>
2021-02-11 19:28:24 -0600

    anotha one

---
+++ 2012-02-11/3/README.md
@@ -0,0 +1,20 @@
+## [[2/11/2012] Challenge #3 [easy]](https://old.reddit.com/r/dailyprogrammer/comments/pjbj8/easy_challenge_2/)
+
+Welcome to cipher day!
+
+write a program that can encrypt texts with an alphabetical caesar cipher. This cipher can ignore numbers, symbols, and whitespace.
+
+for extra credit, add a "decrypt" function to your program!
+
+## Demo
+
+
+```zsh
+$ go run .
+string to encrypt
+hello
+rovvy
+string to decrypt
+rovvy
+hello
+```
---
+++ 2012-02-11/3/easy/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+const cipherOffset = 10
+
+func prompt(prompt string) string {
+	fmt.Println(prompt)
+	var field string
+	_, err := fmt.Scanln(&field)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return field
+}
+
+func encrypt(input string, offset int) {
+	for _, c := range input {
+		fmt.Print(string(int(c) + offset))
+	}
+	fmt.Println()
+}
+
+func decrypt(input string, offset int) {
+	for _, c := range input {
+		fmt.Print(string(int(c) - offset))
+	}
+	fmt.Println()
+}
+
+func main() {
+	input := prompt("string to encrypt")
+	encrypt(input, cipherOffset)
+	input = prompt("string to decrypt")
+	decrypt(input, cipherOffset)
+}
